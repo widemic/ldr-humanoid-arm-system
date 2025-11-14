25e47ca319abb394ab54f2713d35d8201d257737
jeysonTK <alexzamfir94@gmail.com>
2025-11-14 09:33:12 +0200

    correction launcher for  arm demo real perception

--- src/applications/arm_demos/launch/arm_demo_real_perception.launch.py
+++ src/applications/arm_demos/launch/arm_demo_real_perception.launch.py
@@ -56,7 +56,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='camera_to_base_tf',
-            arguments=['0', '-5.0', '1.0', '0.0', '0.0', '-1.57', 'base_fixture_link', 'camera_link'],
+            arguments=['0', '-1.0', '1.0', '-1.57', '0.0', '-1.57', 'base_fixture_link', 'camera_link'],
             output='screen'
         ),
         
@@ -95,8 +95,6 @@ def generate_launch_description():
                     output='screen',
                     parameters=[{'use_sim_time': False}],
                     remappings=[
-                        ('/camera/color/image_raw', '/camera/image_raw'),
-                        ('/camera/depth/image_raw', '/camera/depth/image_raw'), 
                         ('/camera/depth/points', '/camera/points'),
                         ('/camera/depth/camera_info', '/camera/camera_info')
                     ]
@@ -104,21 +102,34 @@ def generate_launch_description():
             ]
         ),
         
-        # # 5. RViz
-        # TimerAction(
-        #     period=LaunchConfiguration('rviz_delay'),
-        #     actions=[
-        #         Node(
-        #             package='rviz2',
-        #             executable='rviz2',
-        #             name='rviz2',
-        #             output='screen',
-        #             arguments=['-d', os.path.join(arm_perception_dir, 'config', 'deep_real_camera.rviz')],
-        #             parameters=[{'use_sim_time': False}]
-        #         )
-        #     ]
-        # ),
+        # 5. RViz
+        TimerAction(
+            period=LaunchConfiguration('rviz_delay'),
+            actions=[
+                Node(
+                    package='rviz2',
+                    executable='rviz2',
+                    name='rviz2',
+                    output='screen',
+                    arguments=['-d', os.path.join(arm_perception_dir, 'config', 'deep_real_camera.rviz')],
+                    parameters=[{'use_sim_time': False}]
+                )
+            ]
+        ),
         
+        # 6. Open Object Detection Node after 1 seconds
+        TimerAction(
+            period=10.0,
+            actions=[
+                Node(
+                    package='arm_perception',  
+                    executable='object_recognition_node.py', 
+                    name='object_recognition_node',
+                    output='screen',
+                    parameters=[{'use_sim_time': True}]
+                )
+            ]
+        ),
         # Logs
         launch.actions.LogInfo(msg="=== Arm Description + External Camera Demo ==="),
         launch.actions.LogInfo(msg="Added static transform from base_fixture_link to camera_link"),
